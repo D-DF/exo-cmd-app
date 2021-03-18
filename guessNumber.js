edbc383c5ba1c189a625346301bb321ca9ea3f2e
D-DF <daviddefreitas0192@gmail.com>
2021-03-19 00:07:36 +0100

    OK

--- guessNumber.js
+++ guessNumber.js
@@ -6,18 +6,21 @@ if(isNaN(numberWin)) {
     console.log(`Ce n'est pas un nombre`)
 }
 
-let reponse; 
+let question;
 
-do {
-    reponse = readlineSync.question('Quel est le juste nombre ?')
-    
-    if(reponse > numberWin) {
-        console.log(chalk.red("Votre chiffre est supérieur"))
-    } else if (numberWin > reponse) {
-        console.log(chalk.red("Votre réponse est inférieur"))
+while (numberWin !== question) {
+
+    question = readlineSync.question('Quel est le juste nombre ? ');
+
+    if(numberWin > question) {
+        console.log(chalk.red("Votre chiffre est plus petit"))
+    } else if (question > numberWin) {
+        console.log(chalk.red('Votre chiffre est plus grand'))
     } else {
-        console.log(chalk.green("Bravo!"));
-    }
-} while (reponse === numberWin)
+        console.log(chalk.green('Bravo'))
+        process.exit(1)
+    }  
+}
 
+    
 
