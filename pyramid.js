af427888481ed85beb2ca1702608961c8d725261
D-DF <daviddefreitas0192@gmail.com>
2021-03-19 14:18:43 +0100

    OK

--- pyramid.js
+++ pyramid.js
@@ -2,19 +2,34 @@ const nbElement = process.argv[2];
 const number    = Number(nbElement);
 const string    = process.argv[3];
 const reverse   = process.argv[4];
+const argvLength = process.argv.length;
+
+if(isNaN(number)) {
+  console.log(`Error: ${number} is not a number.`);
+  process.exit(1);
+}
 
 const showStars = (number, str) => {
-    for (let i = 1; i <= number; i += 1) {
-      console.log(str.repeat(i));
-    }
-
-    if(isNaN(number)) {
-        console.log(`Error: ${number} is not a number.`);
-        process.exit(1);
-    }
+  for (let i = 1; i <= number; i += 1) {
+    console.log(str.repeat(i));
   }
+}
 
-showStars(number, string);
+const showStarsReverse = (element, str) => {
+    for (let i = element; i >= 0; i -= 1) {
+      console.log(str.repeat(i));
+  }
+}
+
+
+if(reverse === "-r") {
+  showStarsReverse(nbElement, string)
+} else {
+  showStars(nbElement, string)
+}
+
+
+
 
 
 
